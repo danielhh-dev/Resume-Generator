e15e79b16c4748701e4cccbf91dc3872227fa8d9
Daniel Alejandro Hernández Gálvez <109681970+DanielHernandezGalvez@users.noreply.github.com>
2023-08-06 23:21:26 -0600

    info completada

--- src/components/CVGenerator.tsx
+++ src/components/CVGenerator.tsx
@@ -8,19 +8,42 @@ import {
   StyleSheet,
 } from "@react-pdf/renderer";
 
+interface Experience {
+  position: string;
+  company: string;
+  since: string;
+  until: string;
+  isWorking: boolean;
+}
+
+interface Education {
+  university: string;
+  degree: string;
+  startDate: string;
+  endDate: string;
+  isOngoing: boolean;
+}
+
 const CVGenerator: React.FC = () => {
   const [name, setName] = useState<string>("");
   const [email, setEmail] = useState<string>("");
   const [phone, setPhone] = useState<string>("");
+  const [description, setDescription] = useState<string>("");
   const [experience, setExperience] = useState<string>("");
   const [company, setCompany] = useState<string>("");
   const [since, setSince] = useState<string>("");
   const [isWorking, setIsWorking] = useState<boolean>(false);
   const [until, setUntil] = useState<string>("");
-  const [education, setEducation] = useState<string>("");
   const [skills, setSkills] = useState<string[]>([]);
   const [newSkill, setNewSkill] = useState<string>("");
   const [resumeName, setResumeName] = useState<string>("");
+  const [experiences, setExperiences] = useState<Array<Experience>>([]);
+  const [educationList, setEducationList] = useState<Array<Education>>([]);
+  const [university, setUniversity] = useState<string>("");
+  const [degree, setDegree] = useState<string>("");
+  const [startDate, setStartDate] = useState<string>("");
+  const [endDate, setEndDate] = useState<string>("");
+  const [isOngoing, setIsOngoing] = useState<boolean>(false);
 
   const addSkill = () => {
     if (newSkill.trim() !== "") {
@@ -33,6 +56,62 @@ const CVGenerator: React.FC = () => {
     setSkills((prevSkills) => prevSkills.filter((_, i) => i !== index));
   };
 
+  const addExperience = () => {
+    if (
+      experience.trim() !== "" &&
+      company.trim() !== "" &&
+      since.trim() !== ""
+    ) {
+      const newExperience: Experience = {
+        position: experience.trim(),
+        company: company.trim(),
+        since: since.trim(),
+        until: until.trim(),
+        isWorking: isWorking,
+      };
+      setExperiences((prevExperiences) => [...prevExperiences, newExperience]);
+      setExperience("");
+      setCompany("");
+      setSince("");
+      setUntil("");
+      setIsWorking(false);
+    }
+  };
+
+  const deleteExperience = (index: number) => {
+    setExperiences((prevExperiences) =>
+      prevExperiences.filter((_, i) => i !== index)
+    );
+  };
+
+  const addEducation = () => {
+    if (
+      university.trim() !== "" &&
+      degree.trim() !== "" &&
+      startDate.trim() !== ""
+    ) {
+      const newEducation: Education = {
+        university: university.trim(),
+        degree: degree.trim(),
+        startDate: startDate.trim(),
+        endDate: isOngoing ? "Present" : endDate.trim(),
+        isOngoing: isOngoing,
+      };
+      setEducationList((prevEducation) => [...prevEducation, newEducation]);
+      setUniversity("");
+      setDegree("");
+      setStartDate("");
+      setEndDate("");
+      setIsOngoing(false);
+    }
+  };
+
+  const deleteEducation = (index: number) => {
+    setEducationList((prevEducation) =>
+      prevEducation.filter((_, i) => i !== index)
+    );
+  };
+
   const generateCV = () => {
     // Create a new PDF document
     const MyDocument: React.FC = () => (
@@ -42,13 +121,31 @@ const CVGenerator: React.FC = () => {
             <Text style={styles.name}>{name}</Text>
             <Text style={styles.content}>{email}</Text>
             <Text style={styles.content}>{phone}</Text>
+            <Text style={styles.content}>Sobre mí: {description}</Text>
             {/* Experience */}
-            <Text style={styles.title}>-{experience}</Text>
-            <Text style={styles.content}>
-              {company}, {since} - {until}
-            </Text>
+            <Text style={styles.title}>Experience:</Text>
+            {experiences.map((exp, index) => (
+              <View key={index}>
+                <Text>{exp.position}</Text>
+                <Text style={styles.content}>
+                  {exp.company}, {exp.since} -{" "}
+                  {exp.isWorking ? "Presente" : exp.until}
+                </Text>
+              </View>
+            ))}
+            {/* Educación */}
             <Text style={styles.title}>Education:</Text>
-            <Text style={styles.content}>{education}</Text>
+            {educationList.map((edu, index) => (
+              <View key={index}>
+                <Text>
+                  {edu.degree} in {edu.university}
+                </Text>
+                <Text style={styles.content}>
+                  {edu.startDate} al {edu.isOngoing ? "Presente" : edu.endDate}
+                </Text>
+              </View>
+            ))}
+            {/* Habilidades */}
             <Text style={styles.title}>Skills:</Text>
             {skills.map((skill, index) => (
               <Text key={index} style={styles.content}>
@@ -91,7 +188,7 @@ const CVGenerator: React.FC = () => {
   });
 
   return (
-    <div className="container mb-5">
+    <div className="container mb-5 pb-5">
       <h1>CV Generator</h1>
       <div className=" d-flex flex-wrap gap-5">
         <section>
@@ -118,40 +215,114 @@ const CVGenerator: React.FC = () => {
           value={phone}
           onChange={(e) => setPhone(e.target.value)}
         />
-        {/* Experience */}
-        <label>Position:</label>
-        <input
-          value={experience}
-          onChange={(e) => setExperience(e.target.value)}
-        />
-        <label>Company:</label>
-        <input value={company} onChange={(e) => setCompany(e.target.value)} />
-        <label>Since:</label>
-        <input
-          value={since}
-          type="date"
-          onChange={(e) => setSince(e.target.value)}
-        />
-
-        <label>I'm working here:</label>
-        <input
-          type="checkbox"
-          checked={isWorking}
-          onChange={(e) => setIsWorking(e.target.checked)}
-        />
-        <label>Until:</label>
-        <input
-          value={until}
-          type="date"
-          onChange={(e) => setUntil(e.target.value)}
-          disabled={isWorking} // Deshabilitar el input si el checkbox es true
-        />
-
-        <label>Education:</label>
+        <label>Presentación:</label>
         <textarea
-          value={education}
-          onChange={(e) => setEducation(e.target.value)}
+          value={description}
+          onChange={(e) => setDescription(e.target.value)}
         />
+        {/* Experience */}
+        <div className="input-group mt-4 gap-4">
+          <label>Position:</label>
+          <input
+            value={experience}
+            onChange={(e) => setExperience(e.target.value)}
+          />
+          <label>Company:</label>
+          <input value={company} onChange={(e) => setCompany(e.target.value)} />
+          <label>Since:</label>
+          <input
+            value={since}
+            type="date"
+            onChange={(e) => setSince(e.target.value)}
+          />
+
+          <label>I'm working here:</label>
+          <input
+            type="checkbox"
+            checked={isWorking}
+            onChange={(e) => setIsWorking(e.target.checked)}
+          />
+          <label>Until:</label>
+          <input
+            value={until}
+            type="date"
+            onChange={(e) => setUntil(e.target.value)}
+            disabled={isWorking}
+          />
+
+          <button className="btn btn-primary" onClick={addExperience}>
+            Add Experience
+          </button>
+        </div>
+
+        <ul className="">
+          {experiences.map((exp, index) => (
+            <li key={index} className="my-3">
+              {exp.position} at {exp.company}, {exp.since} -{" "}
+              {exp.isWorking ? "Presente" : exp.until}
+              <button
+                className="btn btn-danger"
+                onClick={() => deleteExperience(index)}
+              >
+                Delete
+              </button>
+            </li>
+          ))}
+        </ul>
+
+        {/* Educación */}
+        <div className="container mb-5">
+          {/* ... (otro JSX) */}
+          <label>University:</label>
+          <input
+            value={university}
+            onChange={(e) => setUniversity(e.target.value)}
+          />
+
+          <label>Degree:</label>
+          <input value={degree} onChange={(e) => setDegree(e.target.value)} />
+
+          <label>Start Date:</label>
+          <input
+            value={startDate}
+            type="date"
+            onChange={(e) => setStartDate(e.target.value)}
+          />
+
+          <label>Ongoing:</label>
+          <input
+            type="checkbox"
+            checked={isOngoing}
+            onChange={(e) => setIsOngoing(e.target.checked)}
+          />
+
+          <label>End Date:</label>
+          <input
+            value={endDate}
+            type="date"
+            onChange={(e) => setEndDate(e.target.value)}
+            disabled={isOngoing}
+          />
+
+          <button className="btn btn-primary" onClick={addEducation}>
+            Add Education
+          </button>
+
+          <ul>
+            {educationList.map((edu, index) => (
+              <li key={index}>
+                {edu.degree} in {edu.university}, {edu.startDate} -{" "}
+                {edu.isOngoing ? "Present" : edu.endDate}
+                <button
+                  className="btn btn-danger"
+                  onClick={() => deleteEducation(index)}
+                >
+                  Delete
+                </button>
+              </li>
+            ))}
+          </ul>
+        </div>
 
         <label>Skills:</label>
         <textarea

a1030b926e530c7bdc74c9b6f95d16a6763657a7
Daniel Alejandro Hernández Gálvez <109681970+DanielHernandezGalvez@users.noreply.github.com>
2023-08-28 12:31:05 -0600

    footer

--- src/components/other/Footer.tsx
+++ src/components/other/Footer.tsx
@@ -1,7 +1,14 @@
 import React from "react";
 
 const Footer: React.FC = () => {
-  return <footer className="footer">Developed by Daniel Galvez</footer>;
+  return (
+    <footer className="footer">
+      {" "}
+      <p>
+        &copy; 2023 Developed with love by <span>Daniel Galvez</span>
+      </p>
+    </footer>
+  );
 };
 
 export default Footer;
